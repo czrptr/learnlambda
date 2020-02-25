6024a6f9c16303078100244a762bb7428f07fc6f
czrptr <cezar.c.petriuc@gmail.com>
2020-02-25 22:04:00 +0200

    rule based generic tokenizer

---
+++ src/tokenize.ts
@@ -0,0 +1,54 @@
+import "./utils";
+
+export class TokenizeError extends Error {
+    constructor(
+        readonly position: number,
+        message?: string
+    ) {
+		super(message); 
+    }
+    
+    print(): void {
+		console.log(" ".repeat(this.position) + "^");
+		console.error(this.message);
+	}
+}
+
+export interface Token {
+    readonly id: any,
+    readonly start: number;
+    readonly value: string;
+}
+
+export type TokenizeFunction<T extends Token> = (tokens: Array<T>, expression: string, pos: number) => void;
+
+export function tokenize<T extends Token>(expression: string, rules: Array<[RegExp, TokenizeFunction<T>]>): Array<T> {
+    const separator = /^\s+/;
+    
+    let result: Array<T> = [];
+    let temp: Array<T> = [];
+    let i = 0;
+    while (i < expression.length) {
+        let separation = expression.substr(i).match(separator);
+        if (separation) {
+            result = [...result, ...temp];
+            temp = [];
+            i += separation[0].length;
+        } else {
+            let matched = false;
+            for (let rule of rules) {
+                let subexpression = expression.substr(i).match(rule[0]);
+                if (subexpression) {
+                    rule[1](temp, subexpression[0], i);
+                    i += subexpression[0].length;
+                    matched = true;
+                    break;
+                }
+            }
+            if (!matched) {
+                throw new TokenizeError(i, `unexpected character: ${expression[i]}`)
+            }
+        }
+    }
+    return [...result, ...temp];
+}
