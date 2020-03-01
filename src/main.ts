8e8835d5b5c707d8b9986838aefe2d525ad284e0
czrptr <cezar.c.petriuc@gmail.com>
2020-03-01 12:00:22 +0200

    DeBruijn notation testing

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,11 @@ const exprs = [
 	// "x 32",
 	// "x _y",
 	// "g (f (λx.x))",
-	// "g f (λx.x)", // FIX ME
+	// "g f (λx.x)",
 	// "(λx.x) y",
-	// "λx.λy.(x y) ((z w) x)",
+	// "λx.λy.x y (λx.x w)",
+	"(λx. λy. z x (λu. u x)) (λx. w x)",
+	// "λx.λy.x y w",
 	// ".",
 	// "(",
 
@@ -37,11 +39,11 @@ const exprs = [
 	// "λx:[Nat, [Nat, Nat]].x",
 	// "λx:Bool -> (Bool-> Bool).x",
 	// "λx:(Bool -> Bool) -> ((Bool -> Bool) -> Bool).x",
-	"λx:([Nat -> Nat -> Nat, (Bool)]).x",
+	// "λx:([Nat -> Nat -> Nat, (Bool)]).x",
 ];
 
-// import { tokenize, parse } from "./untyped";
-import { tokenize, parse } from "./typed";
+import { tokenize, parse } from "./untyped";
+// import { tokenize, parse } from "./typed";
 
 for (const expr of exprs) {
 	try {
@@ -49,7 +51,7 @@ for (const expr of exprs) {
 		const ast = parse(tokens);
 		// log(tokens + "");
 		// log(ast);
-		log(`"${expr}" -> "${ast}"`);
+		log(`"${expr}" -> "${ast} | ${ast.toDeBruijnString()}"`);
 	} catch (e) {
 		log(expr);
 		if (e instanceof TokenizeError || e instanceof ParseError)
