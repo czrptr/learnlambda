6b142acf402416c83cf1dca2373aafadf9aa3ebc
czrptr <cezar.c.petriuc@gmail.com>
2020-03-20 13:06:31 +0200

    just some tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
-import { TokenizeError, tokenize, ParseError, parse } from "./untyped";
+import { TokenizeError, tokenize, ParseError, parse, subst, evalOnce, evaluate, ExecutionContext } from "./untyped";
 
 const log = console.log;
 
-const expr = "λx.x.";
+function setupContext(ctx: ExecutionContext): void {
+	ctx.addAlias("true", "λx.λy.x");
+	ctx.addAlias("false", "λx.λy.y");
+	ctx.addAlias("not", "λp.p false true");
+	ctx.addAlias("and", "λp.λq.p q p");
+	ctx.addAlias("or", "λp.λq.p p q");
+
+	// ctx.addAlias("zero", "λf.λx.x");
+	ctx.addAlias("succ", "λn.λf.λx.f (n f x)");
+	ctx.addAlias("plus", "λm.λn.m succ n");
+	ctx.addAlias("pow", "λb.λe.e b");
+	ctx.addAlias("one", "succ false");
+	ctx.addAlias("two", "succ one");
+	ctx.addAlias("three", "succ two");
+	ctx.addAlias("four", "succ three");
+	ctx.addAlias("five", "succ four");
+	ctx.addAlias("six", "succ five");
+	ctx.addAlias("seven", "succ six");
+	ctx.addAlias("eight", "succ seven");
+	ctx.addAlias("nine", "succ eight");
+}
+
+let ctx = new ExecutionContext();
+setupContext(ctx);
+
+const expr = "pow four four";
+// const expr = "(λf.λx.f (f (f (f x)))) (λf.λx.f (f (f (f x))))";
 try {
-	const ast = parse(tokenize(expr));
+	console.time("eval");
+	const ast = ctx.evaluate(expr);
+	// const ast = evaluate(parse(tokenize(expr)));
+	console.timeEnd("eval");
+	log(ast.toString());
+	// log(ast.toString().match(/x/g)!.length - 1);
 } catch (e) {
 	if (e instanceof TokenizeError || e instanceof ParseError) {
 		const info = e.toPrint();
