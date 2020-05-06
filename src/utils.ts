46b7b4f36f04e11167f99b5d31da43568ed0019d
czrptr <cezar.c.petriuc@gmail.com>
2020-05-06 15:29:05 +0300

    array equality

--- src/utils.ts
+++ src/utils.ts
@@ -18,3 +18,15 @@ export function last<T>(array: Array<T>): T | undefined {
 export function clamp(n: number, min: number, max: number): number {
 	return Math.min(Math.max(n, min), max);
 }
+
+export function arrayEqual<T>(arr1: T[], arr2: T[]): boolean {
+	if (arr1 === arr2) return true;
+	if (arr1 == undefined || arr2 == undefined) return false;
+	if (arr1.length != arr2.length) return false;
+
+	// clone and sort for order independence equality
+
+	for (let i = 0; i < arr1.length; i += 1)
+		if (arr1[i] !== arr2[i]) return false;
+	return true;
+}
